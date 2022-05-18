9fd2a74fd0179af0031545199e6e5a9632f44a68
Dennis Ojeda <royojeda1@gmail.com>
2022-05-19 04:59:45 +0800

    Add the first test

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,1 +1,7 @@
 require './lib/script.rb'
+
+describe "#caesar_cipher" do
+  it "prints the output of shifting a string by a positive offset" do
+    expect(caesar_cipher('ghijkl', 5)).to eql('lmnopq')
+  end
+end
