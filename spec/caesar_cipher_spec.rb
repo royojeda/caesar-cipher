e6cffb5959119dfebd8aab283ab017340388bcde
Dennis Ojeda <royojeda1@gmail.com>
2022-05-19 05:04:16 +0800

    Add a test for negative offsets with wrapping

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -12,4 +12,8 @@ describe "#caesar_cipher" do
   it "prints the output of shifting a string by a positive offset with wrapping" do
     expect(caesar_cipher('vwxy', 6)).to eql('bcde')
   end
+
+  it "prints the output of shifting a string by a negative offset with wrapping" do
+    expect(caesar_cipher('bbccddee', -4)).to eql('xxyyzzaa')
+  end
 end
