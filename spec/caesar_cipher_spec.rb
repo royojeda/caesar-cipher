ab4c3071d09b2da8981588ee0ae5e26be85b589c
Dennis Ojeda <royojeda1@gmail.com>
2022-05-19 05:01:21 +0800

    Add a test for negative offset

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "prints the output of shifting a string by a positive offset" do
     expect(caesar_cipher('ghijkl', 5)).to eql('lmnopq')
   end
+
+  it "prints the output of shifting a string by a negative offset" do
+    expect(caesar_cipher('pqrstuv', -8)).to eql('hijklmn')
+  end
 end
